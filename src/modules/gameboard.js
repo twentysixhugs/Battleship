92705bba56ca617157ff482476e010c6140b7691
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-05 17:21:46 +0300

    remove gameboard's ship

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -2,31 +2,27 @@ import ShipFactory from "./ship";
 
 function GameboardFactory() {
   const _length = 10; // 10 x 10 board
-  const _gameboard = []; // only ships are stored here
+  const _ships = []; // only ships are stored here
 
   this.placeShip = function (ship) {
-    _gameboard.push(ship);
+    _ships.push(ship);
   }
 
-  //get it
+  this.receiveAttack = function (attackCoordinate) {
+    for (ship of _ships) {
+      for (shipCoordinate of ship.coordinates) {
+        if (shipCoordinate === attackCoordinate) {
+          ship.hit(ship.coordinates.indexOf(shipCoordinate)); // hit the ship at this position (coordinate)
+        }
+      }
+    }
+  }
 
-  this.Ship = function (coordinates) {
-    const shipLength = coordinates.length;
-    const prototype = new ShipFactory(shipLength);
+  function _checkIfHit(checkedCoordinate) {
 
-    return Object.assign(Object.create(prototype), { coordinates });
+    return false;
   }
 
 }
-
-/* class GameboardShipFactory extends ShipFactory {
-  constructor(coordinates) {
-    const shipLength = coordinates.length;
-    super(shipLength);
-
-    this.coordinates = coordinates;
-  }
-} */
-
 
 export default GameboardFactory;
