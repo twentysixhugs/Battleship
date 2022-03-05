51b8755479f9a13c997f1903cafdfd165315abfe
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-05 14:02:52 +0300

    Add 'new' when invoking ShipFactory

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,7 +1,7 @@
 import ShipFactory from '../src/modules/ship';
 
 test('ship hit is marked', () => {
-  const ship = ShipFactory(4); // create a ship with length 4
+  const ship = new ShipFactory(4); // create a ship with length 4
   ship.hit(2); // hit the third position
 
   expect(ship.getPosition(2)).toHaveProperty('isHit', true);
@@ -9,7 +9,7 @@ test('ship hit is marked', () => {
 
 test('ship is sunk when all positions are hit', () => {
   const len = 4;
-  const ship = ShipFactory(len);
+  const ship = new ShipFactory(len);
 
   for (let i = 0; i < len; i++) {
     ship.hit(i);
