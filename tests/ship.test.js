d2b607c1beb1d1b615662ad88fd8c099a4d9c8ac
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-05 10:32:55 +0300

    Revert "add test for hit"
    This reverts commit 0a054cb52976e29aebe6c80a040724dcc90e29b9.

--- tests/ship.test.js
+++
@@ -1,9 +0,0 @@
-import ShipFactory from '../src/modules/ship'
-
-test('ship hit is marked', () => {
-  const ship = ShipFactory(4); // create a ship with length 4
-  ship.hit(2); // hit the third position
-
-  expect(ship.getPosition(2)).toHaveProperty('isHit', true);
-});
-
