2c285e7afec06938c0ecc4cb9fa8b4f56e4232a3
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-06 10:53:18 +0300

    Add missed attacks handling

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -2,7 +2,12 @@ import { hitShip } from "./manager";
 
 function GameboardFactory() {
   const _length = 10; // 10 x 10 board
-  const _ships = []; // only ships are stored here
+  const _ships = [];
+  const _missedAttacks = [];
+
+  function _placeMissedAttack(attackCoordinate) {
+    _missedAttacks.push(attackCoordinate);
+  }
 
   this.placeShips = function (...receivedShips) {
     _ships.push(...receivedShips);
@@ -12,13 +17,22 @@ function GameboardFactory() {
     return [..._ships];
   }
 
+  this.getMissedAttacks = function () {
+    return [..._missedAttacks];
+  }
+
   this.receiveAttack = function (attackCoordinate) {
     for (const ship of _ships) {
       for (const shipCoordinate of ship.getCoordinates()) {
         if (shipCoordinate === attackCoordinate) {
           hitShip(ship, ship.getCoordinates().indexOf(shipCoordinate)); // hit the ship at this position
+          return;
         }
       }
+    }
+
+    if (!_missedAttacks.includes(attackCoordinate)) {
+      _placeMissedAttack(attackCoordinate);
     }
   }
 }
