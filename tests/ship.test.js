df10379d43d53ce8eeea2660840882d858323032
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-06 09:02:25 +0300

    Add test for ship creation

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -9,6 +9,13 @@ test('ship hit is marked', () => {
   expect(ship.getPosition(2)).toHaveProperty('isHit', true);
 });
 
+test('creates an extended ship that has coordinates', () => {
+  const coordinates = ['F1', 'F2', 'F3', 'F4'];
+  const ship = new ShipFactory(coordinates); // create a ship with length 4
+
+  expect(ship).toHaveProperty('coordinates', coordinates);
+});
+
 test('ship is sunk when all positions are hit', () => {
   const coordinates = ['F1', 'F2', 'F3', 'F4'];
   const ship = new ShipFactory(coordinates);
