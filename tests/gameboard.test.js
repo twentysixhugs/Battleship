412e0f8d62e622de2796646e43712d22fee1fee0
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-06 09:03:00 +0300

    Add tests for attack receiving and handling

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -2,19 +2,29 @@ import GameboardFactory from '../src/modules/gameboard';
 import ShipFactory from '../src/modules/ship';
 
 
-describe('new ship creation', () => {
-  const gameboard = new GameboardFactory();
-  const coordinates = ['F5', 'F6', 'F7', 'F8'];
-  const ship = gameboard.Ship(coordinates);
+describe('attack receiving and handling', () => {
+  test('hits the ship cell when attacked', () => {
+    const gameboard = new GameboardFactory();
 
-  test('creates an extended ship that inherits from ShipFactory', () => {
-    expect(ship).toBeInstanceOf(ShipFactory);
+    const ship = new ShipFactory(['F1', 'F2', 'F3', 'F4']);
+    gameboard.placeShip(ship);
+
+    const attackCoordinate = 'F2';
+    gameboard.receiveAttack(attackCoordinate);
+
+    expect(ship.getPosition(1)).toHaveProperty('isHit', true);
   });
 
-  test('creates an extended ship that has coordinates', () => {
-    expect(ship).toHaveProperty('coordinates', coordinates);
-  });
-})
+  test('not attacked cells are not affected', () => {
+    const gameboard = new GameboardFactory();
 
+    const ship = new ShipFactory(['F1', 'F2', 'F3', 'F4']);
+    gameboard.placeShip(ship);
 
+    const attackCoordinate = 'F2';
+    gameboard.receiveAttack(attackCoordinate);
+
+    expect(ship.getPosition(2)).toHaveProperty('isHit', false);
+  })
+});
 
