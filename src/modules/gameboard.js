a6e5373feeb41a4009c39c621c7da22a8228deef
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-06 13:29:37 +0300

    Allow for comparing arrays
    * Instead of comparing previous coordinates that were something like
    * 'F8', 'A2', it now compares the new ones like [6, 8] and [1, 2]

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -22,24 +22,45 @@ function GameboardFactory() {
     _missedAttacks.push(attackCoordinate);
   }
 
+  function _validateShipCoordinates(ship) {
+    // аски значение?
+
+  }
+
+  function _addForbiddenCoordinates(ship) {
+    const shipCoordinates = ship.getCoordinates();
+
+    shipCoordinates.forEach(_getForbiddenCoordinates);
+  }
+
+  function _getForbiddenCoordinates(coordinate) {
+
+  }
+
   this.placeShips = function (...receivedShips) {
+    // if can push return true else return false
     _ships.push(...receivedShips);
   }
 
 
   this.receiveAttack = function (attackCoordinate) {
+    const attackCoordinateStr = attackCoordinate.toString();
     for (const ship of _ships) {
       for (const shipCoordinate of ship.getCoordinates()) {
-        if (shipCoordinate === attackCoordinate) {
+        if (shipCoordinate.toString() === attackCoordinateStr) {
           hitShip(ship, ship.getCoordinates().indexOf(shipCoordinate)); // hit the ship at this position
           return;
         }
       }
     }
 
-    if (!_missedAttacks.includes(attackCoordinate)) {
-      _placeMissedAttack(attackCoordinate);
+    for (const missedAttack of _missedAttacks) {
+      if (missedAttack.toString() === attackCoordinateStr) {
+        return;
+      }
     }
+
+    _placeMissedAttack(attackCoordinate);
   }
 }
 
