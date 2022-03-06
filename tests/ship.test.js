89e66095ad7259b0070e3145d06f4bd1073dd18c
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-06 10:34:44 +0300

    Check for array equality instead of for having property

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -13,7 +13,7 @@ test('creates an extended ship that has coordinates', () => {
   const coordinates = ['F1', 'F2', 'F3', 'F4'];
   const ship = new ShipFactory(...coordinates); // create a ship with length 4
 
-  expect(ship).toHaveProperty('coordinates', coordinates);
+  expect(ship.getCoordinates()).toEqual(coordinates);
 });
 
 test('ship is sunk when all positions are hit', () => {
