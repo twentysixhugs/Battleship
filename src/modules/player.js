88f90294f4d40455d9ed8701cd3d8724fe222468
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-08 11:36:20 +0300

    Integrate PlayerManager and add Computer class

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,4 +1,5 @@
 import Gameboard from "./gameboard";
+import PlayerManager from "./player_manager";
 
 class Player {
   #gameboard;
@@ -6,12 +7,23 @@ class Player {
   constructor(name) {
     this.name = name;
     this.#gameboard = new Gameboard();
+    PlayerManager.addPlayer(this);
   }
 
   get gameboard() {
     return this.#gameboard;
   }
 
+}
+
+class Computer extends Player {
+  constructor() {
+    super('Computer');
+  }
+
+  makeMove() {
+
+  }
 }
 
 
