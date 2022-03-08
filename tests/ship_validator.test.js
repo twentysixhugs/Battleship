ec0b0553af4d80ec0b1b1a321b9c70534a29968d
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-08 18:39:58 +0300

    Fix matcher call

--- tests/ship_validator.test.js
+++ tests/ship_validator.test.js
@@ -10,37 +10,37 @@ describe('ship placement validation', () => {
   test('can place ship on valid coordinates', () => {
     const ship = new ShipFactory([6, 6], [6, 7]);
 
-    expect(validateShipPlacement(ship, ships)).toBeTruthy;
+    expect(validateShipPlacement(ship, ships)).toBeTruthy();
   });
 
   test('cannot place a ship on another ship', () => {
     const ship = new ShipFactory([1, 2], [1, 3]);
 
-    expect(validateShipPlacement(ship, ships)).toBeFalsy;
+    expect(validateShipPlacement(ship, ships)).toBeFalsy();
   })
 
   test('cannot place a ship when crossing the ship', () => {
     const ship = new ShipFactory([2, 3], [3, 3], [4, 3]);
 
-    expect(validateShipPlacement(ship, ships)).toBeFalsy;
+    expect(validateShipPlacement(ship, ships)).toBeFalsy();
   })
 
   test('cannot place a ship adjacent to another ship of the same length', () => {
     const ship = new ShipFactory([2, 2], [2, 3], [2, 4]);
 
-    expect(validateShipPlacement(ship, ships)).toBeFalsy;
+    expect(validateShipPlacement(ship, ships)).toBeFalsy();
   })
 
   test('cannot place a shorter ship adjacent to a longer ship', () => {
     const ship = new ShipFactory([2, 2], [2, 3]);
 
-    expect(validateShipPlacement(ship, ships)).toBeFalsy;
+    expect(validateShipPlacement(ship, ships)).toBeFalsy();
   })
 
   test('cannot place a longer ship adjacent to a shorter ship', () => {
     const ship = new ShipFactory([2, 2], [2, 3], [2, 4], [2, 5]);
 
-    expect(validateShipPlacement(ship, ships)).toBeFalsy;
+    expect(validateShipPlacement(ship, ships)).toBeFalsy();
   })
 })
 
