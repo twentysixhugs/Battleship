e22a86c37a0bfc08aff7869537a6f7c81cc82741
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-08 11:14:43 +0300

    Move here static methods from Player

---
+++ src/modules/player_manager.js
@@ -0,0 +1,26 @@
+const playerManager = (() => {
+  let _current;
+
+  function _toggleCurrent(p1, p2) {
+    _current = (_current === p1) ? p2 : p1;
+  }
+
+  function setCurrent(player) {
+    _current = player;
+  }
+
+  function getCurrent() {
+    return _current;
+  }
+
+  function handleGameboardAttack(attacker, attacked, coordinates) {
+    attacked.gameboard.receiveAttack(coordinates);
+    _toggleCurrent(attacker, attacked);
+  }
+
+  return {
+    setCurrent,
+    getCurrent,
+    handleGameboardAttack,
+  }
+})();
