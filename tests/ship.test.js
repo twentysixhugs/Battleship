3fe9cc8919e645f871b928bcaaa447c7501fee67
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-08 11:37:52 +0300

    Replace ShipFactory call with Ship()

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,8 +1,8 @@
-import ShipFactory from '../src/modules/ship';
+import Ship from '../src/modules/ship';
 
 test('ship hit is marked', () => {
   const coordinates = [[6, 1], [6, 2], [6, 3], [6, 4]];
-  const ship = new ShipFactory(...coordinates); // create a ship with length 4
+  const ship = new Ship(...coordinates); // create a ship with length 4
 
   ship.hit(2); // hit the third position
 
@@ -11,14 +11,14 @@ test('ship hit is marked', () => {
 
 test('creates an extended ship that has coordinates', () => {
   const coordinates = [[6, 1], [6, 2], [6, 3], [6, 4]];
-  const ship = new ShipFactory(...coordinates); // create a ship with length 4
+  const ship = new Ship(...coordinates); // create a ship with length 4
 
   expect(ship.getCoordinates()).toEqual(coordinates);
 });
 
 test('ship is sunk when all positions are hit', () => {
   const coordinates = [[6, 1], [6, 2], [6, 3], [6, 4]];
-  const ship = new ShipFactory(...coordinates);
+  const ship = new Ship(...coordinates);
 
   for (let i = 0; i < coordinates.length; i++) {
     ship.hit(i);
