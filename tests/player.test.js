7e60b1e87fc80cdda3aa71ee6d4bfa0865e49a6e
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-09 11:04:09 +0300

    Add tests for ship attack start and stop

--- tests/player.test.js
+++ tests/player.test.js
@@ -27,6 +27,41 @@ describe('Human player', () => {
 });
 
 describe('Computer player', () => {
+  test('computer stops attacking the ship once it is sunk', () => {
+    const player = new Player('1');
+    const computer = new Computer();
+
+    player.gameboard.placeShip(new Ship([2, 2], [2, 3], [2, 4], [2, 5]));
+
+    PlayerManager.setCurrent(computer);
+
+    player.gameboard.receiveAttack([2, 5]); // hit a ship
+    computer.tryingToSinkShip = true;
+    computer.firstHitAtShip = [2, 5];
+    player.gameboard.receiveAttack([2, 4]); // sink it
+    player.gameboard.receiveAttack([2, 3]);
+    player.gameboard.receiveAttack([2, 2]);
+
+    computer.defineNextMove();
+
+    expect(computer.tryingToSinkShip).toBeFalsy();
+    expect(computer.firstHitAtShip).toBeFalsy();
+  });
+
+  test('Computer starts attacking a ship once it has hit one of ship\'s positions', () => {
+    const player = new Player('1');
+    const computer = new Computer();
+
+    PlayerManager.setCurrent(computer);
+
+    player.gameboard.placeShip(new Ship([2, 2], [2, 3], [2, 4], [2, 5]));
+    player.gameboard.receiveAttack([2, 5]);
+
+    computer.defineNextMove();
+
+    expect(computer.tryingToSinkShip).toBeTruthy();
+    expect(computer.firstHitAtShip).toBeTruthy();
+  });
   test('When trying to sink a ship, computer does not attack the surrounding and same cells if they are already attacked', () => {
     const player = new Player('1');
     const computer = new Computer();
