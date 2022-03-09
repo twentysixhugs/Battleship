ebcce34f2b6951bde22f3e06828e32fd848d8158
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-09 14:53:58 +0300

    Write the functions starting game

---
+++ src/modules/game.js
@@ -0,0 +1,51 @@
+import { Computer, Player } from "./player";
+import PlayerManager from "./player_manager";
+import Input from "./user_input";
+import Ship from "./ship";
+
+const Game = (() => {
+  let _gameGoing = false;
+  let _winner = null;
+
+  function start() {
+    let player = new Player(Input.getPlayerName());
+    let computer = new Computer();
+
+    PlayerManager.addPlayer(player);
+    PlayerManager.addPlayer(computer);
+    PlayerManager.setCurrent(player);
+
+    _placeShips(player, computer);
+
+    _gameGoing = true;
+  }
+
+
+
+  function _placeShips(player1, player2) {
+    // TODO: 
+    // Loop over data from the input
+    // Place it.
+    /* Validation must be done in input, this function should only place valid data */
+    player1.gameboard.placeShip(new Ship([[1, 1], [1, 2], [1, 3], [1, 4]]));
+    player1.gameboard.placeShip(new Ship([[3, 1], [3, 2], [3, 3]]));
+    player1.gameboard.placeShip(new Ship([[2, 7], [3, 7], [4, 7]]));
+    player1.gameboard.placeShip(new Ship([[7, 3], [8, 3]]));
+    player1.gameboard.placeShip(new Ship([[1, 10], [2, 10]]));
+    player1.gameboard.placeShip(new Ship([[8, 9], [9, 9]]));
+
+    player2.gameboard.placeShip(new Ship([[1, 1], [1, 2], [1, 3], [1, 4]]));
+    player2.gameboard.placeShip(new Ship([[3, 1], [3, 2], [3, 3]]));
+    player2.gameboard.placeShip(new Ship([[2, 7], [3, 7], [4, 7]]));
+    player2.gameboard.placeShip(new Ship([[7, 3], [8, 3]]));
+    player2.gameboard.placeShip(new Ship([[1, 10], [2, 10]]));
+    player2.gameboard.placeShip(new Ship([[8, 9], [9, 9]]));
+  }
+
+  return {
+    start,
+    respondToMove,
+  }
+})();
+
+export default Game;
