b554e88b37e22261978dab3c8769e57ebc82d0c7
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-10 11:39:08 +0300

    Add module for start menu

---
+++ src/modules/dom/start_menu.js
@@ -0,0 +1,13 @@
+function addEventsToButtons(cbRandom, cbReset, cbStart) {
+  const random = document.querySelector('.js-random');
+  const reset = document.querySelector('.js-reset');
+  const start = document.querySelector('.js-start');
+
+  random.addEventListener('click', cbRandom);
+  reset.addEventListener('click', cbReset);
+  start.addEventListener('click', cbStart.bind(null));
+}
+
+export {
+  addEventsToButtons,
+}
