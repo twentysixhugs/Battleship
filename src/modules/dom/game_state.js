0e04490602e097317c39825712d6dd177525a148
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-10 12:50:37 +0300

    Change the interface when the game starts

--- src/modules/dom/game_state.js
+++ src/modules/dom/game_state.js
@@ -2,9 +2,12 @@ import { playerTurn, computerTurn } from "./current_player";
 
 const UIGameState = (() => {
   let _currentPlayer;
+  let _isStartGame = true;
 
   function startGame() {
     _currentPlayer = 'player';
+    _toggleStartGameInterface();
+    _showBothBattlefields();
     playerTurn();
   }
 
@@ -16,6 +19,7 @@ const UIGameState = (() => {
 
   }
 
+
   function toggleCurrentPlayer() {
     if (_currentPlayer === 'player') {
       _currentPlayer = 'computer';
@@ -24,6 +28,45 @@ const UIGameState = (() => {
       _currentPlayer = 'player';
       playerTurn();
     }
+  }
+
+  function _showBothBattlefields() {
+
+  }
+
+  function _toggleStartGameInterface() {
+    _toggleStartGameInterfaceVisibility();
+
+    if (_isStartGame) {
+      _disableStartGameInterface();
+      _isStartGame = false;
+    } else {
+      _enableStartGameInterface();
+      _isStartGame = true;
+    }
+  }
+
+  function _disableStartGameInterface() {
+    const buttonsWithListeners = document.querySelectorAll('.js-random, js-reset, js-start');
+
+    buttonsWithListeners.forEach(button => {
+      let buttonWithoutListener = button.cloneNode(true);
+      button.parentNode.replaceChild(buttonWithoutListener, button);
+    });
+  }
+
+  function _enableStartGameInterface() {
+
+  }
+
+  function _toggleStartGameInterfaceVisibility() {
+    const port = document.querySelector('.js-port');
+    const computerGameboard = document.querySelector('.js-computer-gameboard');
+    const gameboardButtons = document.querySelector('.js-gameboard-buttons');
+
+    port.classList.toggle('is-visible');
+    computerGameboard.classList.toggle('is-visible');
+    gameboardButtons.classList.toggle('is-visible');
   }
 
   return {
