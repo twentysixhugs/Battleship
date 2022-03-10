69080bf201a967717de05f33be37c8d7ba7bd861
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-10 11:16:53 +0300

    Add functions to get and set last move

--- src/modules/input.js
+++ src/modules/input.js
@@ -1,5 +1,21 @@
-const Input = (() => {
+import { convertElementsToNumbers } from "./helper";
 
-});
+const Input = (() => {
+  let _lastMove;
+
+  function setLastMove(coordinate) {
+    _lastMove = convertElementsToNumbers(coordinate);
+    console.log(_lastMove);
+  }
+
+  function getLastMove() {
+    return _lastMove;
+  }
+
+  return {
+    setLastMove,
+    getLastMove
+  }
+})();
 
 export default Input;
