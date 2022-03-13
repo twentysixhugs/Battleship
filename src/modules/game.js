6994f7db36236950aeb4a2442e2de6267cec3943
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-13 11:54:08 +0300

    Call moves from UIGameState

--- src/modules/game.js
+++ src/modules/game.js
@@ -3,7 +3,6 @@ import PlayerManager from "./player_manager";
 import Input from "./input";
 import Ship from "./ship";
 import UIGameState from "./dom/game_state";
-import { playerMove, computerMove } from "./dom/battlefield";
 
 const Game = (() => {
   let _gameGoing = false;
@@ -44,20 +43,21 @@ const Game = (() => {
       const attacker = PlayerManager.getCurrent();
       const attacked = PlayerManager.getNotCurrent();
 
-      if (!attacked.gameboard.isLastAttackSuccessful()) {
-        continue;
-      }
 
       if (attacked.isGameOver()) {
         _stop();
         _winner = attacker;
         console.log("winner: ", _winner);
+        UIGameState.stopGame();
         UIGameState.showGameResult(_winner === player ? 'win' : 'lose');
         break;
       }
 
+      if (!attacked.gameboard.isLastAttackSuccessful()) {
+        continue;
+      }
+
       if (!attacked.gameboard.lastAttackHitShip()) {
-        console.log('last attack did not hit ship')
         PlayerManager.toggleCurrent();
         UIGameState.toggleCurrentPlayer();
       }
@@ -65,10 +65,10 @@ const Game = (() => {
 
     async function nextMove() {
       if (PlayerManager.getCurrent() === player) {
-        await playerMove(player);
+        await UIGameState.playerMove(player);
       }
       else if (PlayerManager.getCurrent() === computer) {
-        await computerMove(computer);
+        await UIGameState.computerMove(computer);
       }
     }
   }
@@ -79,8 +79,10 @@ const Game = (() => {
     // Place it.
     /* Validation must be done in input, this function should only place valid data */
     player1.gameboard.placeShip(new Ship([1, 1], [1, 2], [1, 3], [1, 4]));
+    player1.gameboard.placeShip(new Ship([5, 4], [5, 5], [5, 6]));
 
     player2.gameboard.placeShip(new Ship([1, 1], [1, 2], [1, 3], [1, 4]));
+    player2.gameboard.placeShip(new Ship([5, 4], [5, 5], [5, 6]));
   }
 
   return {
