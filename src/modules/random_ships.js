6d2f93a9866ddb1d563136e78462f48611a043c4
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-14 18:46:37 +0300

    Build random ships generation

--- src/modules/random_ships.js
+++ src/modules/random_ships.js
@@ -5,35 +5,50 @@ import {
 import Input from "./input";
 import Ship from "./ship";
 
-function generateShipsRandomly() {
-  const generatedShips = [
-    getValidShip(4),
-    getValidShip(3),
-    getValidShip(3),
-    getValidShip(2),
-    getValidShip(2),
-    getValidShip(2),
-    getValidShip(1),
-    getValidShip(1),
-    getValidShip(1),
-    getValidShip(1),
-  ];
-
-  Input.placeShips(generatedShips);
+function generateShipsForBothPlayers() {
+  generateShipsRandomly();
+  generateShipsRandomly();
 }
 
-function getValidShip(shipLength) {
+function generateShipsRandomly() {
+  const readyShips = [
+  ];
+
+  const carrier = getValidShip(4, readyShips);
+  readyShips.push(carrier);
+
+  for (let i = 0; i < 2; i++) {
+    const battleship = getValidShip(3, readyShips);
+    readyShips.push(battleship);
+  }
+
+  for (let i = 0; i < 3; i++) {
+    const cruiser = getValidShip(2, readyShips);
+    readyShips.push(cruiser);
+  }
+
+  for (let i = 0; i < 4; i++) {
+    const patrolBoat = getValidShip(1, readyShips);
+    readyShips.push(patrolBoat);
+  }
+
+  Input.placeShips(readyShips);
+}
+
+function getValidShip(shipLength, allShips) {
   while (true) {
     const generatedShip = generateShip(shipLength);
 
-    if (validateRelativeShipPlacement(generateShip)) {
+    if (validateRelativeShipPlacement(generatedShip, allShips)) {
       return generatedShip;
     }
   }
 }
 
 function generateShip(shipLength) {
-  const firstCoordinate = getValidPlacementCells(shipLength);
+  const validCells = getValidPlacementCells(shipLength);
+
+  const firstCoordinate = validCells[Math.floor(Math.random() * validCells.length)];
   const coordinateX = firstCoordinate[0];
   const coordinateY = firstCoordinate[1];
 
@@ -46,4 +61,5 @@ function generateShip(shipLength) {
   return new Ship(...shipCoordinates);
 }
 
-export default generateShipsRandomly;
+export default generateShipsForBothPlayers;
+export { generateShipsRandomly }
