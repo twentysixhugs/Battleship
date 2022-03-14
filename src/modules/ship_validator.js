2bbd453d9338f3ed2d0282d2d75e9fb98a7fd4dc
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-14 18:48:06 +0300

    Validate ships to not overflow the gameboard

--- src/modules/ship_validator.js
+++ src/modules/ship_validator.js
@@ -3,23 +3,28 @@ import { getCellsSurroundingShip, stringifyElements } from './helper';
 /* The purpose of this module is to not allow to place ships */
 /* Adjacent to each other. There must be some space between them */
 
-function validateShipPlacement(validatedShip, ships) {
-  const shipCells = stringifyElements(validatedShip.getCoordinates());
-  const forbiddenCoordinates = stringifyElements(getForbiddenCoordinates(ships));
+/* First, it defines whether or not the placement is valid relative to other ships on board */
+/* Second, it checks whether or not the coordinates are not outside the battlefield */
 
+function validateRelativeShipPlacement(validatedShip, ships) {
+  /* Validate against other ships */
 
-  if (shipCells.some(cell => forbiddenCoordinates.includes(cell))) {
+  const shipCells =
+    stringifyElements(validatedShip.getCoordinates());
+
+  const adjacentShipCoordinates =
+    stringifyElements(getAdjacentShipCoordinates(ships));
+
+  if (shipCells.some(cell => adjacentShipCoordinates.includes(cell))) {
     return false;
   }
 
   return true;
 }
 
-export default validateShipPlacement;
 
-
-function getForbiddenCoordinates(ships) {
-  const forbiddenCoordinates = ships
+function getAdjacentShipCoordinates(ships) {
+  const adjacentShipCoordinates = ships
     .map(ship => {
       const shipCoordinates = ship.getCoordinates();
       return getCellsSurroundingShip(shipCoordinates);
@@ -28,8 +33,84 @@ function getForbiddenCoordinates(ships) {
 
   ships.forEach(ship => {
     const shipCoordinates = ship.getCoordinates();
-    shipCoordinates.forEach(coordinate => forbiddenCoordinates.push(stringifyElements(coordinate)));
+    shipCoordinates.forEach(coordinate => adjacentShipCoordinates.push(stringifyElements(coordinate)));
   });
 
-  return forbiddenCoordinates;
+  return adjacentShipCoordinates;
 }
+
+function getValidPlacementCells(validatedShipLength) {
+  const validPlacementCells = [];
+
+  switch (validatedShipLength) {
+    case 4: {
+      validPlacementCells.push(...getCellsValidForShipFour());
+      break;
+    }
+    case 3: {
+      validPlacementCells.push(...getCellsValidForShipThree());
+      break;
+    }
+    case 2: {
+      validPlacementCells.push(...getCellsValidForShipTwo());
+      break;
+    }
+    case 1: {
+      validPlacementCells.push(...getAllBoard());
+      break;
+    }
+  }
+
+  return validPlacementCells;
+}
+
+function getCellsValidForShipFour() {
+  const validCells = [];
+
+  for (let x = 1; x <= 7; x++) {
+    for (let y = 1; y < 10; y++) {
+      validCells.push([x, y]);
+    }
+  }
+
+  return validCells;
+}
+
+function getCellsValidForShipThree() {
+  const validCells = [];
+
+  for (let x = 1; x <= 8; x++) {
+    for (let y = 1; y < 10; y++) {
+      validCells.push([x, y]);
+    }
+  }
+
+  return validCells;
+}
+
+function getCellsValidForShipTwo() {
+  const validCells = [];
+
+  for (let x = 1; x <= 9; x++) {
+    for (let y = 1; y <= 10; y++) {
+      validCells.push([x, y]);
+    }
+  }
+
+  return validCells;
+}
+
+function getAllBoard() {
+  const allBoard = [];
+
+  for (let x = 1; x <= 10; x++) {
+    for (let y = 1; y <= 10; y++) {
+      allBoard.push([x, y]);
+    }
+  }
+
+  return allBoard;
+}
+
+
+export { validateRelativeShipPlacement, getValidPlacementCells }
