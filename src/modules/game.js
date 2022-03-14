6d2aa5a9ed1c7ec8fc13c80b535c20783bfd7ad6
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-14 18:19:39 +0300

    Add ship placement

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,9 +1,10 @@
-import { Computer, Player } from "./player";
 import PlayerManager from "./player_manager";
 import Input from "./input";
 import Ship from "./ship";
 import UIGameState from "./dom/game_state";
+import generateShipsForBothPlayers from "./random_ships";
 import { showHitAtShip, showMissedAttack, showSunkShip } from "./dom/battlefield";
+import { Computer, Player } from "./player";
 
 const Game = (() => {
   let _gameGoing = false;
@@ -22,6 +23,7 @@ const Game = (() => {
 
   function _stop() {
     _gameGoing = false;
+    Input.clear();
   }
 
   function _initPlayers() {
@@ -59,7 +61,7 @@ const Game = (() => {
       if (attacked.isGameOver()) {
         _stop();
         _winner = attacker;
-        console.log("winner: ", _winner);
+
         UIGameState.stopGame();
         UIGameState.showGameResult(_winner === player ? true : false);
         break;
@@ -76,16 +78,12 @@ const Game = (() => {
     }
   }
 
-  function _placeShips(player1, player2) {
-    // TODO: 
-    // Loop over data from the input
-    // Place it.
-    /* Validation must be done in input, this function should only place valid data */
-    player1.gameboard.placeShip(new Ship([1, 1], [1, 2], [1, 3], [1, 4]));
-    player1.gameboard.placeShip(new Ship([5, 4], [5, 5], [5, 6]));
+  function _placeShips(player, computer) {
+    const playerShips = Input.getPlayerShips();
+    const computerShips = Input.getComputerShips();
 
-    player2.gameboard.placeShip(new Ship([1, 1], [1, 2], [1, 3], [1, 4]));
-    player2.gameboard.placeShip(new Ship([5, 4], [5, 5], [5, 6]));
+    playerShips.forEach(ship => player.gameboard.placeShip(ship));
+    computerShips.forEach(ship => computer.gameboard.placeShip(ship));
   }
 
   return {
